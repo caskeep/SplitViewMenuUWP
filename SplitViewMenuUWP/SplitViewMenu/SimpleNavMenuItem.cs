187d425a6e699df0bc8430da0fed2503912b14f7
diasnicolas <ndias@presidency.com>
2016-02-19 18:01:15 -0200

    added suport to Font Awesome icons.

--- SplitViewMenuUWP/SplitViewMenu/SimpleNavMenuItem.cs
+++ SplitViewMenuUWP/SplitViewMenu/SimpleNavMenuItem.cs
@@ -5,9 +5,14 @@ namespace SplitViewMenu
 {
     public sealed class SimpleNavMenuItem : INavigationMenuItem
     {
+        public SimpleNavMenuItem()
+        {
+            FontFamilySymbol = FontFamily.SegoeMDL2;
+        }
+
         public string Label { get; set; }
-        public Symbol Symbol { get; set; }
-        public char SymbolAsChar => (char) Symbol;
+        public string Symbol { get; set; }
+        public FontFamily FontFamilySymbol { get; set; }
         public object Arguments { get; set; }
         public Type DestinationPage { get; set; }
     }
